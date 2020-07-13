e352c7a147647295543c8d26ab13ac4865c24a79
nonatip <nonatip99@gmail.com>
2020-07-13 23:21:00 +0900

    [add]ControllerManager.cs [add]TestCube.cs [add]TestText.cs [add]InputManager [add]testscene

---
+++ Assets/S_Forder/S_Script/ControllerManager.cs
@@ -0,0 +1,297 @@
+﻿using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    static float leftStickHorizontal, leftStickVertical;
+    static float rightStickHorizontal, rightStickVertical;
+    static float crossHorizontal, crossVertical;
+
+    float a;
+
+    static float normLeftStickHorizontal, normLeftStickVertical;
+    static float normRightStickHorizontal, normRightStickVertical;
+
+    public void Update()
+    {
+        //左スティックの入力値を取得
+        leftStickHorizontal = Input.GetAxis("LeftStickHorizontal");
+        leftStickVertical = Input.GetAxis("LeftStickVertical");
+        //左スティックの入力値を正規化
+        a = Mathf.Sqrt((leftStickHorizontal * leftStickHorizontal) + (leftStickVertical * leftStickVertical));
+        if (a > 0)
+        {
+            a = 1.0f / a;
+        }
+        normLeftStickHorizontal = leftStickHorizontal * a;
+        normLeftStickVertical = leftStickVertical * a;
+
+        //右スティックの入力値を取得
+        rightStickHorizontal = Input.GetAxis("RightStickHorizontal");
+        rightStickVertical = Input.GetAxis("RightStickVertical");
+        //右スティックの入力値を正規化
+        a = Mathf.Sqrt((rightStickHorizontal * rightStickHorizontal) + (rightStickVertical * rightStickVertical));
+        if (a > 0)
+        {
+            a = 1.0f / a;
+        }
+        normRightStickHorizontal = rightStickHorizontal * a;
+        normRightStickVertical = rightStickVertical * a;
+
+        ////十字キーの入力値を取得
+        //crossHorizontal = Input.GetAxis("CrossHorizontal");
+        //crossVertical = Input.GetAxis("CrossHorizontal");
+        ////十字キーの入力値を正規化
+    }
+
+    //左スティックを上に傾けている判定
+    public bool LeftStickUp()
+    {
+        bool check = false;
+
+        if (Mathf.Cos(67.5f) >= normLeftStickHorizontal && normLeftStickHorizontal > Mathf.Cos(112.5f))
+        {
+            if (normLeftStickVertical > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを下に傾けている判定
+    public bool LeftStickDown()
+    {
+        bool check = false;
+
+        if (Mathf.Cos(67.5f) >= normLeftStickHorizontal && normLeftStickHorizontal > Mathf.Cos(112.5f))
+        {
+            if (normLeftStickVertical < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを右に傾けている判定
+    public bool LeftStickRight()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-22.5f) <= normLeftStickVertical && normLeftStickVertical < Mathf.Sin(22.5f))
+        {
+            if (normLeftStickHorizontal > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを左に傾けている判定
+    public bool LeftStickLeft()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-22.5f) <= normLeftStickVertical && normLeftStickVertical < Mathf.Sin(22.5f))
+        {
+            if (normLeftStickHorizontal < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを右上に傾けている判定
+    public bool LeftStickRightUp()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(22.5f) <= normLeftStickVertical && normLeftStickVertical < Mathf.Sin(67.5f))
+        {
+            if (normLeftStickHorizontal > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを左上に傾けている判定
+    public bool LeftStickLeftUp()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(22.5f) <= normLeftStickVertical && normLeftStickVertical < Mathf.Sin(67.5f))
+        {
+            if (normLeftStickHorizontal < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを右下に傾けている判定
+    public bool LeftStickRightDown()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-67.5f) <= normLeftStickVertical && normLeftStickVertical < Mathf.Sin(-22.5f))
+        {
+            if (normLeftStickHorizontal > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //左スティックを左下に傾けている判定
+    public bool LeftStickLeftDown()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-67.5f) <= normLeftStickVertical && normLeftStickVertical < Mathf.Sin(-22.5f))
+        {
+            if (normLeftStickHorizontal < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+
+    //右スティックを上に傾けている判定
+    public bool RightStickUp()
+    {
+        bool check = false;
+
+        if (Mathf.Cos(67.5f) <= normRightStickHorizontal && normRightStickHorizontal < Mathf.Cos(112.5f))
+        {
+            if (normRightStickVertical > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを下に傾けている判定
+    public bool RightStickDown()
+    {
+        bool check = false;
+
+        if (Mathf.Cos(67.5f) <= normRightStickHorizontal && normRightStickHorizontal < Mathf.Cos(112.5f))
+        {
+            if (normRightStickVertical < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを右に傾けている判定
+    public bool RightStickRight()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-22.5f) <= normRightStickVertical && normRightStickVertical < Mathf.Sin(22.5f))
+        {
+            if (normRightStickHorizontal > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを左に傾けている判定
+    public bool RightStickLeft()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-22.5f) <= normRightStickVertical && normRightStickVertical < Mathf.Sin(22.5f))
+        {
+            if (normRightStickHorizontal < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを右上に傾けている判定
+    public bool RightStickRightUp()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(22.5f) <= normRightStickVertical && normRightStickVertical < Mathf.Sin(67.5f))
+        {
+            if (normRightStickHorizontal > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを左上に傾けている判定
+    public bool RightStickLeftUp()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(22.5f) <= normRightStickVertical && normRightStickVertical < Mathf.Sin(67.5f))
+        {
+            if (normRightStickHorizontal < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを右下に傾けている判定
+    public bool RightStickRightDown()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-67.5f) <= normRightStickVertical && normRightStickVertical < Mathf.Sin(-22.5f))
+        {
+            if (normRightStickHorizontal > 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+    //右スティックを左下に傾けている判定
+    public bool RightStickLeftDown()
+    {
+        bool check = false;
+
+        if (Mathf.Sin(-67.5f) <= normRightStickVertical && normRightStickVertical < Mathf.Sin(-22.5f))
+        {
+            if (normRightStickHorizontal < 0)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+
+    public float GetNormLeftStickHorizontal()
+    {
+        return normLeftStickHorizontal;
+    }
+
+    public float GetNormLeftStickVertical()
+    {
+        return normLeftStickVertical;
+    }
+}
---
+++ Assets/S_Forder/S_Script/TestText.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class TestText : MonoBehaviour
+{
+    [SerializeField] private ControllerManager cm;
+
+    public Text text; // Textオブジェクト
+    public Text Horizontal, Vertical;
+
+    // 初期化
+    void Start()
+    {
+    }
+
+    // 更新
+    void Update()
+    {
+        text.text = "入力なし";
+
+        cm.Update();
+
+        float hori = cm.GetNormLeftStickHorizontal();
+        float ver = cm.GetNormLeftStickVertical();
+
+        if (cm.LeftStickUp())
+        {
+            text.text = "左スティック：上";
+        }
+        if(cm.LeftStickDown())
+        {
+            text.text = "左スティック：下";
+        }
+        if (cm.LeftStickRight())
+        {
+            text.text = "左スティック：右";
+        }
+        if (cm.LeftStickLeft())
+        {
+            text.text = "左スティック：左";
+        }
+        if (cm.LeftStickRightUp())
+        {
+            text.text = "左スティック：右上";
+        }
+        if (cm.LeftStickRightDown())
+        {
+            text.text = "左スティック：右下";
+        }
+        if (cm.LeftStickLeftUp())
+        {
+            text.text = "左スティック：左上";
+        }
+        if (cm.LeftStickLeftDown())
+        {
+            text.text = "左スティック：左下";
+        }
+
+        Horizontal.text = "Horizontal:" + hori.ToString();
+        Vertical.text = "Vertical:" + ver.ToString();
+    }
+}
