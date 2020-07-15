1692c4de26ba6f3c9467471d47c371f0fc23ab9e
hopehopehope <hope03282000@icloud.com>
2020-07-16 00:08:53 +0900

    Cameraの動きとテスト用のPlayerの動きを追加

---
+++ Assets/T_Forder/script/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject targetPoint;  //目標オブジェクト
+    public float speed = 0.1f;      //補間スピード
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //**********************************************************
+        //ターゲット方向へ向く
+        //**********************************************************
+
+        // ターゲット方向のベクトルを取得
+        Vector3 targetPosition = targetPoint.transform.position - this.transform.position;
+        // 方向を、回転情報に変換
+        Quaternion targetRotation = Quaternion.LookRotation(targetPosition);
+        // 現在の回転情報と、ターゲット方向の回転情報を補完する
+        transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, speed);
+
+
+    }
+}
---
+++ Assets/T_Forder/script/PlayerMove.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float stdSpeed = 30.0f;
+
+    private Transform camTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTrans = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Z方向速度決定
+        float speed = stdSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed *= 1.5f;
+        }
+        //回転速度決定
+        float rotSpeed=100.0f;
+
+        Vector3 dirForward = new Vector3(camTrans.forward.x, 0.0f, camTrans.forward.z);
+        dirForward.Normalize();
+        Vector3 dirRight = new Vector3(camTrans.right.x, 0.0f, camTrans.right.z);
+        dirForward.Normalize();
+
+        
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += Vector3.forward * Time.deltaTime * speed;
+            //transform.position += dirForward * Time.deltaTime * speed;
+            
+            transform.Rotate(Vector3.left*Time.deltaTime* rotSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position -= Vector3.forward * Time.deltaTime * speed;
+            //transform.position -= dirForward * Time.deltaTime * speed;
+            
+            transform.Rotate(-Vector3.left * Time.deltaTime * rotSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position += Vector3.left * Time.deltaTime * speed;
+            //transform.position -= dirRight * Time.deltaTime * speed;
+            
+            transform.Rotate(-Vector3.up * Time.deltaTime * rotSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position -= Vector3.left * Time.deltaTime * speed;
+            //transform.position += dirRight * Time.deltaTime * speed;
+            
+            transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //transform.position += Vector3.up * Time.deltaTime * speed;
+            //transform.position += camTrans.up * Time.deltaTime * speed;
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            //transform.position -= Vector3.up * Time.deltaTime * speed;
+            //transform.position -= camTrans.up * Time.deltaTime * speed;
+        }
+
+        
+
+        transform.rotation.Normalize();
+
+        //カメラの回転をそのままぶち込む
+        //transform.rotation = camTrans.rotation;
+
+    }
+}
